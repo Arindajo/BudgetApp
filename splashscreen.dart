d075646ff71fda908631eefb8c1c4088e0038048
Arinda <144229704+Arindajo@users.noreply.github.com>
2025-08-29 01:09:13 +0300

    introduced splashscreen

---
+++ splashscreen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'navbar.dart';
+import 'dart:async';
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({super.key});
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  @override
+  void initState(){
+    super.initState();
+    Timer(Duration(seconds:5),(){
+     
+      Navigator.pushReplacement(
+         context,
+         MaterialPageRoute(builder: (context)=>Navbar())
+      );
+    });
+  }
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+       CircleAvatar(
+        backgroundImage: AssetImage("assets/images/logo.jpeg"),
+        radius:40
+       ),
+       Text("TrackFunds",style:TextStyle(fontSize:35,fontWeight:FontWeight.bold),textAlign:TextAlign.center),
+       SizedBox(height:10),
+       Text("Spend Wisely, Save Effortlessly",style:TextStyle(fontSize:20),textAlign:TextAlign.center)
+
+      ],)
+    );
+  }
+}
