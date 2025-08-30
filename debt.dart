ef8023b92b50a040fc6a08014f6dc77de6ea179a
Arinda <144229704+Arindajo@users.noreply.github.com>
2025-08-30 22:09:39 +0300

    small changes

--- debt.dart
+++ debt.dart
@@ -1,53 +1,91 @@
 import 'package:flutter/material.dart';
 
-class Debt extends StatelessWidget {
-  const Debt({super.key});
+class Debt extends StatefulWidget {
+  @override
+  State<Debt> createState() => _DebtState();
+}
+
+class _DebtState extends State<Debt> {
+  List<String> DebtOption=[
+    "Debtor","Creditor"
+  ];
+
+ String? selectedOption;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Column(
+      body:Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+             DropdownButtonFormField<String>(
+             value:selectedOption,
+             hint:Text("Select type"),
+             items:DebtOption.map((String option){
+              return DropdownMenuItem<String>(
+               value:option,
+               child:Text(option)
+              );
+             }).toList(),
 
-        children: [
-          TextField(
-            decoration:InputDecoration(
-              labelText:"Debt Amount",
-              hintText:"Enter Amount",
-              border:OutlineInputBorder()
-            )
-          ),
-          SizedBox(height:15),
+             onChanged: (value){
+              setState((){
+               selectedOption=value!;
+              });
+             },
+             decoration: InputDecoration(
+              border:OutlineInputBorder(),
+              label:Text("Debt Type")
+             ),
+             
+              
+              
+              
+              
+              ),
+            SizedBox(height:15),
             TextField(
-            decoration:InputDecoration(
-              labelText:"Name",
-              hintText:"Enter Name",
-              border:OutlineInputBorder()
+              decoration:InputDecoration(
+                labelText:"Debt Amount",
+                hintText:"Enter Amount",
+                border:OutlineInputBorder()
+              )
+            ),
+            SizedBox(height:15),
+              TextField(
+              decoration:InputDecoration(
+                labelText:"Name",
+                hintText:"Enter Name",
+                border:OutlineInputBorder()
+              )
+            ),
+            SizedBox(height:15),
+              TextField(
+              decoration:InputDecoration(
+                labelText:"Information",
+                hintText:"Enter details",
+                border:OutlineInputBorder()
+              )
+            ),
+            SizedBox(height:15),
+            Row(
+              children:[
+            ElevatedButton(
+              child:Text("Edit"),
+              onPressed: (){}, 
+              
+            ),SizedBox(width:100),
+            ElevatedButton(
+              child:Text("Save"),
+              onPressed: (){}, 
+              
             )
-          ),
-          SizedBox(height:15),
-            TextField(
-            decoration:InputDecoration(
-              labelText:"Information",
-              hintText:"Enter details",
-              border:OutlineInputBorder()
+              ]
             )
-          ),
-          SizedBox(height:15),
-          Row(
-            children:[
-          ElevatedButton(
-            child:Text("Edit"),
-            onPressed: (){}, 
-            
-          ),SizedBox(width:10),
-          ElevatedButton(
-            child:Text("Save"),
-            onPressed: (){}, 
-            
-          )
-            ]
-          )
-        ],
+          ],
+        ),
       )
     );
   }
