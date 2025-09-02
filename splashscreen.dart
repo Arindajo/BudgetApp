d1c61162f23211483ec3e3519dea4622b83836f1
Arinda <144229704+Arindajo@users.noreply.github.com>
2025-09-02 23:42:19 +0300

    small changes

--- splashscreen.dart
+++ splashscreen.dart
@@ -25,13 +25,18 @@ class _SplashscreenState extends State<Splashscreen> {
       body:Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-       CircleAvatar(
-        backgroundImage: AssetImage("assets/images/logo.jpeg"),
-        radius:40
+       Padding(
+        padding:EdgeInsets.all(10.0),
+        
+         child: CircleAvatar(
+          
+          backgroundImage: AssetImage("assets/images/logo.jpeg"),
+          radius:40
+         ),
        ),
        Text("TrackFunds",style:TextStyle(fontSize:35,fontWeight:FontWeight.bold),textAlign:TextAlign.center),
        SizedBox(height:10),
-       Text("Spend Wisely, Save Effortlessly",style:TextStyle(fontSize:20),textAlign:TextAlign.center)
+       Text("Spend Wisely, Save ffortlessly",style:TextStyle(fontSize:20),textAlign:TextAlign.center)
 
       ],)
     );
