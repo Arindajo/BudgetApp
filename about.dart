3268e745bbd1a2390607d71ce5730210edf4575d
Arinda <144229704+Arindajo@users.noreply.github.com>
2025-09-06 00:06:39 +0300

    Add files via upload

--- about.dart
+++ about.dart
@@ -7,11 +7,24 @@ class About extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body:Center(
-        child: Column(children: [
-          Text("More Information About Us"),
-
-          Text("call the Developer: 0770947655")
-        ],),
+        child: SingleChildScrollView(
+          child: Column(children: [
+            CircleAvatar(
+              //logo
+             backgroundImage:  AssetImage("assets/images/logo.jpeg"),
+              radius:40
+            ),
+            Text("TrackFunds",style: TextStyle(fontWeight:FontWeight.bold,fontSize:25),),
+            SizedBox(
+            height:25
+          ),
+          Text("AppVersion:0.0.1",style:TextStyle(fontWeight: FontWeight.bold)),
+          SizedBox(
+            height:25
+          ),
+            Text("call the Developer: 0770947655",style:TextStyle(fontWeight:FontWeight.bold))
+          ],),
+        ),
       )
     );
   }
