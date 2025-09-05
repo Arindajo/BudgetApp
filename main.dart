4c947ff2f99103841b3bf16f1a2b23d719feed16
Arinda <144229704+Arindajo@users.noreply.github.com>
2025-09-05 23:54:39 +0300

    new changes

--- main.dart
+++ main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'debt.dart';
 import 'settings.dart';
 import 'sign.dart';
 import 'home.dart';
@@ -10,19 +10,34 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeMode _mode = ThemeMode.system;
+   void setThemeMode(ThemeMode newMode) {
+    setState(() => _mode = newMode);
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme:ThemeData.light(),
+      darkTheme:ThemeData.dark(),
+      themeMode:_mode ,
+     
       debugShowCheckedModeBanner: false,
       routes:{
-        '/settings':(context)=>Settings(),
+        '/settings':(context)=>Settings(
+          currentMode: _mode,
+          onModeChanged: setThemeMode
+        ),
         '/sign':(context)=>Sign(),
         '/home':(context)=>Home(),
-     
+        '/debt':(context)=>Debt(),
         '/transaction':(context)=>AddTransaction(),
         '/about':(context)=>About()
       },
