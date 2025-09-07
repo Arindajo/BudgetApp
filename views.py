f277870d5a4a025916de6567ce8d154a7aad5d7e
Arinda <144229704+Arindajo@users.noreply.github.com>
2025-09-07 23:54:05 +0300

    new

--- views.py
+++ views.py
@@ -1,8 +1,15 @@
-
-from rest_framework import generics
-from .serializers import SignupSerializer
 from django.contrib.auth.models import User
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-class SignupView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = SignupSerializer
+@api_view(["POST"])
+def signup(request):
+    username = request.data.get("username")
+    email = request.data.get("email")
+    password = request.data.get("password")
+
+    if User.objects.filter(username=username).exists():
+        return Response({"error": "Username already exists"}, status=400)
+
+    user = User.objects.create_user(username=username, email=email, password=password)
+    return Response({"message": "User created successfully"})
