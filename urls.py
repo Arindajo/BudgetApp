91076e0be5bbd26bd42a6d3d488ca4a8b38d2952
Arinda <144229704+Arindajo@users.noreply.github.com>
2025-09-08 16:39:58 +0300

    newly new

--- urls.py
+++ urls.py
@@ -4,6 +4,9 @@ from .views import SignupView
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+
+
